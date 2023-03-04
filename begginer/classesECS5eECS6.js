47a4eda224966d93a327d99b167f090a1e221365
petersonproenca <81689486+petersonproenca@users.noreply.github.com>
2023-03-04 05:10:51 -0300

    Create classesECS5eECS6.js

---
+++ begginer/classesECS5eECS6.js
@@ -0,0 +1,12 @@
+//Diferenças entre ECS5 e ECS6
+//ECS5 - uso de prototypes
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob);
+  this.getFullName = () => {
+    return `${firstName} ${lastName}` 
+  }
+  this.protorype.getYearofBirth = () => {
+    reuturn 
+}
